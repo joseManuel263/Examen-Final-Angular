deb6201df4705adecadb85c216ae812b27acee27
hibi I NOVA <84207744+joseManuel263@users.noreply.github.com>
2024-08-29 11:10:32 -0600

    Update final

--- src/app/time-ago.pipe.ts
+++ src/app/time-ago.pipe.ts
@@ -10,14 +10,18 @@ export class TimeAgoPipe implements PipeTransform {
 
     const date = moment(value);
     const now = moment();
+    const minutesAgo = now.diff(date, 'minutes');
+    const hoursAgo = now.diff(date, 'hours');
     const daysAgo = now.diff(date, 'days');
 
-    if (daysAgo === 0) {
-      return 'Hoy';
+    if (minutesAgo < 60) {
+      return `Hace ${minutesAgo} ${minutesAgo === 1 ? 'minuto' : 'minutos'}`;
+    } else if (hoursAgo < 24) {
+      return `Hace ${hoursAgo} ${hoursAgo === 1 ? 'hora' : 'horas'}`;
     } else if (daysAgo === 1) {
-      return 'Hace 1 día';
+      return 'Ayer';
     } else {
-      return `Hace ${daysAgo} días`;
+      return `Hace ${daysAgo} ${daysAgo === 1 ? 'día' : 'días'}`;
     }
   }
 }
